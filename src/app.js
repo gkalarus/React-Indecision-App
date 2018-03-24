64c860bc98407bde6c428ead5b08c06bef6d365c
Grzegorz Kalarus <kalarus.g@gmail.com>
2018-03-24 13:38:31 +0100

    removing individual options

--- src/app.js
+++ src/app.js
@@ -3,18 +3,21 @@ class ToDoApp extends React.Component {
     super(props);
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
     this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this)
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
     this.state = {
-      options: []
+      options: props.options
     }
   }
 
   handleDeleteOptions() {
-    this.setState(() => {
-      return {
-        options: []
-      }
-    })
+    this.setState(()=>({ options: []}));
+  }
+
+  handleDeleteOption(optionToRemove) {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option)
+    }))
   }
   
   handlePick() {
@@ -29,25 +32,22 @@ class ToDoApp extends React.Component {
       return 'This option already exsist';
     }
 
-    this.setState((prevState) => {
-      return {
-        options: prevState.options.concat([option])
-      };
-    })
+    this.setState((prevState)=>({options: prevState.options.concat(option)}))
+
   }
 
   render() {
 
-    const title = 'To Do App';
     const subtitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle}/>
+        <Header subtitle={subtitle}/>
         <Action handlePick={this.handlePick} hasOptions={this.state.options.length > 0}/>
         <Options 
           options={this.state.options}
-          handleDeleteActions={this.handleDeleteOptions}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
         />
         <AddOption 
           handleAddOption={this.handleAddOption}
@@ -57,52 +57,65 @@ class ToDoApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    );
-  }
+ToDoApp.defaultProps = {
+  options: []
 }
 
-class Action extends React.Component {
-
-
-  render() {
-    return (
-      <div>
-        <button 
-        disabled={!this.props.hasOptions} 
-        onClick={this.props.handlePick}>
-          What should I do?
-        </button>
-      </div>
-    );
-  }
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+  );
 }
 
-class Options extends React.Component {
-  
-  render() {
-    return (
-      <div>
-        {this.props.options.map((option) => <Option key={option} optionText={option}/>)}
-        <button onClick={this.props.handleDeleteActions}>Remove All</button>
-      </div>
-    );
-  }
+Header.defaultProps = {
+  title: 'To Do App'
 }
 
-class Option extends React.Component {
-  render() {
-    return (
-      <div>Option: {this.props.optionText}</div>
-    );
-  }
+const Action = (props) => {
+  return (
+    <div>
+      <button 
+      disabled={!props.hasOptions} 
+      onClick={props.handlePick}>
+        What should I do?
+      </button>
+    </div>
+  );
 }
+
+const Options = (props) => {
+  return (
+    <div>
+      {props.options.map((option) => (
+        <Option 
+          key={option} 
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))}
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+    </div>
+  );
+}
+
+const Option = (props) => {
+  return (
+    <div>
+      {props.optionText}
+      <button 
+        onClick={(e)=>{
+          props.handleDeleteOption(props.optionText);
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
+
 
 class AddOption extends React.Component {
   constructor(props) {
@@ -118,10 +131,8 @@ class AddOption extends React.Component {
       const option = e.target.elements.option.value.trim();
       const error = this.props.handleAddOption(option)
 
-      this.setState(() => {
-        return {error}
-      })
-      
+      this.setState(() => ({error}));
+            
     }
 
   render() {
@@ -137,8 +148,6 @@ class AddOption extends React.Component {
     );
   }
 }
-
-
 
 
 ReactDOM.render(<ToDoApp />, document.getElementById("app"));
