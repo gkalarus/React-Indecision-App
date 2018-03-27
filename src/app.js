8e4b0e0878ee2e5cecf63ada9009fff3281d3328
Grzegorz Kalarus <kalarus.g@gmail.com>
2018-03-27 12:52:31 +0200

    setting up Webpack with SCSS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ToDoApp from './components/ToDoApp'
+import ToDoApp from './components/ToDoApp';
+import './styles/styles.scss';
 
 ReactDOM.render(<ToDoApp />, document.getElementById("app"));
 
